26091e173992b01d3c7ebea0aa36f310e6270c5f
zhangpeng <1132960613@qq.com>
2022-07-11 14:41:52 +0800

    update stack

--- stack/stack.go
+++ stack/stack.go
@@ -1,16 +1,35 @@
 package stack
 
+import "errors"
+
 type Stack struct {
 	Arr []interface{}
 	Cap int
 }
 
+func (s *Stack) IsEmpty() bool {
+	return s.Cap == 0
+}
+
+func (s *Stack) Size() int {
+	return s.Cap
+}
+
+func (s *Stack) Top() interface{} {
+	if s.IsEmpty() {
+		panic(errors.New("stack is empty"))
+	}
+	return s.Arr[s.Cap-1]
+}
+
 func (s *Stack) Pop() interface{} {
-	if s.Cap == 0 {
-		return nil
+	if s.IsEmpty() {
+		panic(errors.New("stack is empty"))
 	}
 	s.Cap--
-	return s.Arr[s.Cap]
+	ret := s.Arr[s.Cap]
+	s.Arr = s.Arr[:s.Cap]
+	return ret
 }
 
 func (s *Stack) Push(obj interface{}) {
